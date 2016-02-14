a9272bf94333cb2830da033dd0b7461c779e9ad7
andersonmcook <andersonmcook@gmail.com>
2016-02-14 01:11:06 -0600

    made clicker code much better

--- scripts/main.js
+++ scripts/main.js
@@ -1,35 +1,16 @@
 'use strict';
-const bg = document.getElementById('bg');
-const pink = document.getElementsByClassName('pink');
-const green = document.getElementsByClassName('green');
-const purple = document.getElementsByClassName('purple');
-const orange = document.getElementsByClassName('orange');
-// const yellow = document.getElementsByClassName('yellow');
-// const white = document.getElementsByClassName('white');
-const darkviolet = document.getElementsByClassName('darkviolet');
-const lightblue = document.getElementsByClassName('lightblue');
 
+// sets soft-light blend mode to background
 function softLight (color) {
   bg.style.background = `url("../img/bg.idea-rev.png"), ${color}`;
   bg.style.backgroundBlendMode = `soft-light`;
 }
 
-pink[0].addEventListener("click", () => {softLight("pink")});
-pink[1].addEventListener("click", () => {softLight("pink")});
+// event listener for clicker
+document.addEventListener("click", clicker);
 
-green[0].addEventListener("click", () => {softLight("green")});
-green[1].addEventListener("click", () => {softLight("green")});
-
-purple[0].addEventListener("click", () => {softLight("purple")});
-purple[1].addEventListener("click", () => {softLight("purple")});
-
-orange[0].addEventListener("click", () => {softLight("orange")});
-orange[1].addEventListener("click", () => {softLight("orange")});
-
-// yellow[0].addEventListener("click", () => {softLight("yellow")});
-
-// white[0].addEventListener("click", () => {softLight("white")});
-
-darkviolet[0].addEventListener("click", () => {softLight("darkviolet")});
-
-lightblue[0].addEventListener("click", () => {softLight("lightblue")});
+// grabs color attribute and passes it into softLight()
+function clicker (event) {
+  const color = event.target.getAttribute('color');
+  softLight(color);
+}
