3fc56308c1f4f76913575fb687349ea067a103a0
andersonmcook <andersonmcook@gmail.com>
2016-02-28 11:19:42 -0600

    styling changes

--- scripts/main.js
+++ scripts/main.js
@@ -8,11 +8,15 @@ function softLight (color) {
   bg.style.backgroundBlendMode = `soft-light`;
 }
 
+function reverse () {
+  bg.style.background = 'url("../img/bg.idea.png")';
+}
+
 // event listener for clicker
 document.addEventListener("click", clicker);
 
 // grabs color attribute and passes it into softLight()
 function clicker (event) {
   const color = event.target.getAttribute('color');
-  softLight(color);
+  color !== 'yellow' ? softLight(color) : reverse();
 }
