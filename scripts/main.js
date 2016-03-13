5915f7853ec54d8e4ce9c1bfc7b4247261dd3ada
andersonmcook <andersonmcook@gmail.com>
2016-03-12 18:53:22 -0600

    indentation and comments

--- scripts/main.js
+++ scripts/main.js
@@ -1,70 +1,70 @@
 ;(function () {
   'use strict';
 
-$(document).ready(function() {
+  $(document).ready(function() {
 
-  // say hello
-  console.log('%c contact me at andersonmcook@gmail.com ', 'background: black; color: white; font-family: monospace; font-size: 1.5em');
+// say hello
+    console.log('%c contact me at andersonmcook@gmail.com ', 'background: black; color: white; font-family: monospace; font-size: 1.5em');
 
-  // store options
-  const options = {
-    revbg: 'url("../img/bg.idea-rev.png")',
-    blend: 'soft-light',
-    initial: 'initial',
-    normbg: 'url("../img/bg.idea.png")',
-    size: 'cover',
-    repeat: 'no-repeat'
-  };
+// store options
+    const options = {
+      revbg: 'url("../img/bg.idea-rev.png")',
+      blend: 'soft-light',
+      initial: 'initial',
+      normbg: 'url("../img/bg.idea.png")',
+      size: 'cover',
+      repeat: 'no-repeat'
+    };
 
-  // sets soft-light blend mode to background
-  function softLight (color) {
-    bg.style.background = `${options.revbg}, ${color}`;
-    bg.style.backgroundBlendMode = options.blend;
-    bg.style.backgroundSize = options.size;
-    bg.style.backgroundRepeat = options.repeat;
-    container.style.mixBlendMode = options.blend;
+// sets soft-light blend mode to background
+    function softLight (color) {
+      bg.style.background = `${options.revbg}, ${color}`;
+      bg.style.backgroundBlendMode = options.blend;
+      bg.style.backgroundSize = options.size;
+      bg.style.backgroundRepeat = options.repeat;
+      container.style.mixBlendMode = options.blend;
 
-    if (color === 'white') {
+      if (color === 'white') {
+        container.style.mixBlendMode = options.initial;
+      }
+    }
+
+// reverse
+    function reverse () {
+      container.style.mixBlendMode = options.initial;
+      bg.style.background = options.normbg;
+      bg.style.backgroundSize = options.size;
+      bg.style.backgroundRepeat = options.repeat;
+    }
+
+// reset
+    function reset () {
+      bg.style.background = options.normbg;
+      bg.style.backgroundBlendMode = options.initial;
+      bg.style.backgroundSize = options.size;
+      bg.style.backgroundRepeat = options.repeat;
       container.style.mixBlendMode = options.initial;
     }
-  }
 
-  // reverse
-  function reverse () {
-    container.style.mixBlendMode = options.initial;
-    bg.style.background = options.normbg;
-    bg.style.backgroundSize = options.size;
-    bg.style.backgroundRepeat = options.repeat;
-  }
+// event listener for clicker
+    document.addEventListener("click", clicker);
 
-  // reset
-  function reset () {
-    bg.style.background = options.normbg;
-    bg.style.backgroundBlendMode = options.initial;
-    bg.style.backgroundSize = options.size;
-    bg.style.backgroundRepeat = options.repeat;
-    container.style.mixBlendMode = options.initial;
-  }
-
-  // event listener for clicker
-  document.addEventListener("click", clicker);
-
-  // grabs color attribute and passes it into softLight()
-  function clicker (event) {
-    const color = event.target.getAttribute('color');
-    if (color !== null) {
-      color !== 'yellow' ? softLight(color) : reverse();
-    } else if (event.target.tagName === 'HTML'){
-      reset();
+// grabs color attribute and passes it into softLight()
+    function clicker (event) {
+      const color = event.target.getAttribute('color');
+      if (color !== null) {
+        color !== 'yellow' ? softLight(color) : reverse();
+      } else if (event.target.tagName === 'HTML'){
+        reset();
+      }
     }
-  }
 
-  // enables tooltips
-  $('[data-toggle="tooltip"]').tooltip({
-    animation: false,
-    placement: "bottom",
-    trigger: "hover"
+// enables tooltips
+    $('[data-toggle="tooltip"]').tooltip({
+      animation: false,
+      placement: "bottom",
+      trigger: "hover"
+    });
+
   });
-
-});
 })();
